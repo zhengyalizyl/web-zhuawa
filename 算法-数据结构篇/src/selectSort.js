6d2211a8786a26bd07397a8ac75e0993ea24a6c0
lizheng <lizheng123lily@timelinelabs.cn>
2023-09-12 21:19:13 +0800

    add

--- 算法-数据结构篇/src/selectSort.js
+++ 算法-数据结构篇/src/selectSort.js
@@ -1,19 +1,28 @@
-function  selectSort(arr){
-  const len =arr.length;
+function selectSort(arr) {
+  const len = arr.length;
   let minIdx;
-  for(let i=0;i<len-1;i+=1){
-    minIdx= i;
-    for(let j=i;j<len;j+=1){
-      if(arr[j]<arr[minIdx]){
-         minIdx = j;
+  //0 ~ len-1
+  //1 ~  n-1
+  //2 ~ n-1
+  for (let i = 0; i < len; i += 1) {
+    //0 ~ len-1
+    //1 ~  n-1
+    //2 ~ n-1
+    //i ~ n-1
+    // 假设最小值是在i位置上
+    minIdx = i;
+    for (let j = i; j < len; j += 1) {
+      if (arr[j] < arr[minIdx]) {
+        minIdx = j;
       }
     }
-     if(minIdx!=i){
-      [arr[i],arr[minIdx]] =[arr[minIdx],arr[i]]
-     }
+    //需要最小的值所在的位置和i所在的位置交换
+    if (minIdx != i) {
+      [arr[i], arr[minIdx]] = [arr[minIdx], arr[i]]
+    }
   }
   return arr;
 }
 
 
-const arr=[2,6,5,9,3,1,8,4,7]
+const arr = [2, 6, 5, 9, 3, 1, 8, 4, 7]
