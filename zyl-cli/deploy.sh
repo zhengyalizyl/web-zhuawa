4a40ce6da0fd50b7175fede772629add69d6842b
zyl <863344241@qq.com>
2024-09-19 11:38:04 +0800

    fix : fix deploy.sh

---
+++ zyl-cli/deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# 输出异常信息
+set -e
+
+push_addr=`git remote get_url --push origin`
+commit_info=`git describr --all -always --long`
+dist_path=docs/.vuepress/dist
+push_branch=gh-pages
+
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
+cd $dist_path
+
+git init
+git add -A
+git commit -m "deploy,$commit_info"
+git push -f $push_addr HEAD:$push_branch
+
+cd -
+rm -rf $dist_path
