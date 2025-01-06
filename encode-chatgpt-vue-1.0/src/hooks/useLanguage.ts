4126497f33d0262b0ac8b50f6078ae27cc7e3289
zhengyali <zhengyali@avenir.hk>
2025-01-06 10:49:42 +0800

    add  vue 项目

---
+++ encode-chatgpt-vue-1.0/src/hooks/useLanguage.ts
@@ -0,0 +1,22 @@
+import { computed } from 'vue'
+import { enUS, zhCN } from 'naive-ui'
+import { useAppStore } from '@/store'
+import { setLocale } from '@/locales'
+
+export function useLanguage() {
+  const appStore = useAppStore()
+
+  const language = computed(() => {
+    setLocale(appStore.language)
+    switch (appStore.language) {
+      case 'en-US':
+        return enUS
+      case 'zh-CN':
+        return zhCN
+      default:
+        return enUS
+    }
+  })
+
+  return { language }
+}
